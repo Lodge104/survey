d3de22ee6bea2b3c6bc105d453ca445546bddc96
Nicholas Anderson <nickanderson1998@gmail.com>
2023-05-27 11:51:38 -0400

    Upgrade to v6.x

--- application/config/version.php
+++ application/config/version.php
@@ -11,10 +11,10 @@
  * See COPYRIGHT.php for copyright notices and details.
  */
 
-$config['versionnumber'] = '5.4.7';
-$config['dbversionnumber'] = 491;
-$config['buildnumber'] = 221019;
+$config['versionnumber'] = '6.1.0';
+$config['dbversionnumber'] = 606;
+$config['buildnumber'] = 230522;
 $config['updatable'] = true;
 $config['templateapiversion']  = 3;
-$config['assetsversionnumber'] = '30302';
+$config['assetsversionnumber'] = '30337';
 return $config;
--- application/views/responses/browseindex_view.php
+++ application/views/responses/browseindex_view.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Response Summary view
  * @var $this AdminController
@@ -15,48 +16,48 @@ echo viewHelper::getViewTestTag('surveyResponsesIndex');
 <div class='side-body <?php echo getSideBodyClass(true); ?>'>
     <h3><?php eT("Response summary"); ?></h3>
     <div class="row">
-        <div class="col-lg-12 content-right">
-            <table class='statisticssummary table'>
+        <div class="col-12 content-right">
+            <table class='ls-statisticssummary table'>
                 <tbody>
-                <tr>
-                    <th><?php eT("Full responses"); ?></th>
-                    <td><?php echo $num_completed_answers; ?></td>
-                </tr>
-                <tr>
-                    <th><?php eT("Incomplete responses"); ?></th>
-                    <td><?php echo($num_total_answers - $num_completed_answers); ?></td>
-                </tr>
+                    <tr>
+                        <th><?php eT("Full responses"); ?></th>
+                        <td><?php echo $num_completed_answers; ?></td>
+                    </tr>
+                    <tr>
+                        <th><?php eT("Incomplete responses"); ?></th>
+                        <td><?php echo ($num_total_answers - $num_completed_answers); ?></td>
+                    </tr>
+                    <tr class="ls-statisticssummary__sum">
+                        <th><?php eT("Total responses"); ?></th>
+                        <td><?php echo $num_total_answers; ?></td>
+                    </tr>
                 </tbody>
-                <tr>
-                    <th><?php eT("Total responses"); ?></th>
-                    <td><?php echo $num_total_answers; ?></td>
-                </tr>
             </table>
         </div>
     </div>
-    <?php if (isset($with_token)): ?>
+    <?php if (isset($with_token)) : ?>
         <h3><?php eT("Survey participant summary"); ?></h3>
         <div class="row">
-            <div class="col-lg-12 content-right">
-                <table class='statisticssummary table'>
+            <div class="col-12 content-right">
+                <table class='ls-statisticssummary table'>
                     <tbody>
-                    <tr>
-                        <th><?php eT("Total invitations sent"); ?></th>
-                        <td><?php echo $tokeninfo['sent']; ?></td>
-                    </tr>
-                    <tr>
-                        <th><?php eT("Total surveys completed"); ?></th>
-                        <td><?php echo $tokeninfo['completed']; ?></td>
-                    </tr>
-                    <tr>
-                        <th><?php eT("Total with no unique access code"); ?></th>
-                        <td><?php echo $tokeninfo['invalid'] ?></td>
-                    </tr>
+                        <tr>
+                            <th><?php eT("Total invitations sent"); ?></th>
+                            <td><?php echo $tokeninfo['sent']; ?></td>
+                        </tr>
+                        <tr>
+                            <th><?php eT("Total surveys completed"); ?></th>
+                            <td><?php echo $tokeninfo['completed']; ?></td>
+                        </tr>
+                        <tr>
+                            <th><?php eT("Total with no unique access code"); ?></th>
+                            <td><?php echo $tokeninfo['invalid'] ?></td>
+                        </tr>
+                        <tr class="ls-statisticssummary__sum">
+                            <th><?php eT("Total records"); ?></th>
+                            <td><?php echo $tokeninfo['count']; ?></td>
+                        </tr>
                     </tbody>
-                    <tr>
-                        <th><?php eT("Total records"); ?></th>
-                        <td><?php echo $tokeninfo['count']; ?></td>
-                    </tr>
                 </table>
             </div>
         </div>
